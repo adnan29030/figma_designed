b3b0336d21c669ef7173b0e1a7a8c45e23ac7f42
adnan29030 <smg29030@gmail.com>
2024-11-20 23:28:57 +0500

    first commit

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,15 +1,20 @@
 import Link from "next/link";
+interface HeaderParams {
+    font: {
+        className:string;
+    };
+  }
 
 
 
-export default function Header(data:any) {
+export default function Header({font} : HeaderParams) {
     return (
         <>
-        <div className= {`${data.font.className} ul-parents`}>
+        <div className= {`${font.className} ul-parents`}>
             <ul className="links">
-                <li className="li-li"><Link href="">Works</Link></li>
-                <li className="li-li"><Link href="">Blog</Link></li>
-                <li className="li-li"><Link href="">Contact</Link></li>
+                <li className="li-li"><Link href="#">Works</Link></li>
+                <li className="li-li"><Link href="#">Blog</Link></li>
+                <li className="li-li"><Link href="#">Contact</Link></li>
             </ul>
         </div>
         </>
